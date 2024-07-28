fc7ec20bb6cb49ff7d64c336b04a211c44a826fc
dharmil desai <dharmil@elsner.com>
2024-07-28 16:51:04 +0530

    add

--- routes/web.php
+++ routes/web.php
@@ -61,6 +61,5 @@ Route::post('/electricity/store', [ElectricityQuestionController::class, 'store'
 Route::post('/other/store', [OtherQuestionController::class, 'store'])->name('other-questions.store');
 Route::post('/road/store', [RoadQuestionController::class, 'store'])->name('road-questions.store');
 Route::post('/sewer/store', [SewerQuestionController::class, 'store'])->name('sewer-questions.store');
-
-
+Route::post('/revenue/store', [RevenueQuestionController::class, 'store'])->name('revenue-questions.store');
 // Storing Routes Starts END
